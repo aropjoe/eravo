f8e6e983434285eb4d7ec586e1c75f9cb434141c
AropJoe <josephbusa@outlook.com>
2023-08-18 10:43:09 +0100

    write new utility functions

--- eravo/core/utils.py
+++ eravo/core/utils.py
@@ -25,3 +25,26 @@ def fetch_vulnerability_data(software_name, software_version):
             vulnerabilities.append(vulnerability)
 
     return vulnerabilities
+
+
+def generate_security_report(target_type, target_value):
+    # Fetch data from VirusTotal and other sources
+    report_data = fetch_virustotal_data(target_type, target_value)
+    # Create SecurityReport entry
+    security_report = SecurityReport.objects.create(target_type=target_type, target_value=target_value)
+    # Create ScanResult entries
+    for data_source, data in report_data.items():
+        ScanResult.objects.create(security_report=security_report, data_source=data_source, report_data=data)
+    return security_report
+
+
+def analyze_malicious_item(malicious_item):
+    # Fetch data from security data sources
+    detection_result = fetch_detection_result(malicious_item.item_type, malicious_item.value)
+    malicious_item.detection_result = detection_result
+    malicious_item.save()
+
+def fetch_detection_result(item_type, value):
+    # Implement logic to fetch detection result using security data sources
+    # Return a dictionary containing detection insights
+    return {"insights": "detection_insights"}
