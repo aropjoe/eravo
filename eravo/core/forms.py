7e2fd7ed44afaf92addd49c6b64ca1256c02bdcb
AropJoe <josephbusa@outlook.com>
2023-08-18 10:41:48 +0100

    add new form classes

--- eravo/core/forms.py
+++ eravo/core/forms.py
@@ -3,3 +3,20 @@ from django import forms
 
 class ScanForm(forms.Form):
     sha256 = forms.CharField(max_length=64, required=True)
+
+
+class SecurityReportForm(forms.Form):
+    target_type = forms.ChoiceField(choices=[('file', 'File'), ('url', 'URL'), ('domain', 'Domain'), ('ip', 'IP Address')])
+    target_value = forms.CharField(max_length=255)
+
+
+class IncidentForm(forms.ModelForm):
+    class Meta:
+        model = Incident
+        fields = ('description', 'status')
+
+
+class MaliciousItemForm(forms.ModelForm):
+    class Meta:
+        model = MaliciousItem
+        fields = ('item_type', 'value', 'detection_result')
