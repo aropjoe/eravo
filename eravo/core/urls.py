53fd7cdf89c5a93767cdb19a9c1519542e2c1207
AropJoe <josephbusa@outlook.com>
2023-08-18 10:26:56 +0100

    add new URL route

--- eravo/core/urls.py
+++ eravo/core/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
     path('populate-scan-results/', views.populate_scan_results, name='populate_scan_results'),
+    path('api/analyze-apps/', views.analyze_installed_apps, name='analyze_installed_apps'),
 ]
