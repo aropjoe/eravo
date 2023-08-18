ad77667a6f119d25012319ce9f23514aec2b61bc
AropJoe <josephbusa@outlook.com>
2023-08-18 10:26:43 +0100

    write new models for Software and Vulnerability

--- eravo/core/models.py
+++ eravo/core/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class ScanResult(models.Model):
     scan_id = models.CharField(max_length=100, unique=True)
     timestamp = models.DateTimeField(auto_now_add=True)
@@ -8,10 +9,25 @@ class ScanResult(models.Model):
     threat_name = models.CharField(max_length=100)
     scan_engine = models.CharField(max_length=100)
 
+
 class GeographicOrigin(models.Model):
     country = models.CharField(max_length=100)
     count = models.PositiveIntegerField()
 
+
 class IndustryTarget(models.Model):
     industry = models.CharField(max_length=100)
     count = models.PositiveIntegerField()
+
+
+class Software(models.Model):
+    name = models.CharField(max_length=100)
+    version = models.CharField(max_length=20)
+    # Add more fields as needed
+
+
+class Vulnerability(models.Model):
+    software = models.ForeignKey(Software, on_delete=models.CASCADE)
+    description = models.TextField()
+    severity = models.CharField(max_length=10)
+    # Add more fields as needed
